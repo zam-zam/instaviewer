82a5f37be8ce5f8bfe4a1ae9fb9378331b4fbb27
zamzam <web@zamolod.ru>
2017-08-29 02:30:50 +0100

    media page changes

--- core/pages/media.php
+++ core/pages/media.php
@@ -1,7 +1,7 @@
 <?php 
 $mediaId = $match['params']['mediaId'];
 $media = $insta->getMediaInfo($mediaId);
-
+// var_dump($media->items[0]);exit();
 $template = $twig->load($match['name'].'.html');
 echo $template->render(array('media' => $media->items[0]));
 ?>
